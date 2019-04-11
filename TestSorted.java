6123b0531aec23784e79874c8cac4b64979f7a8c
LarsHMa <larswiebach@hotmail.de>
2019-04-11 17:36:45 +0200

    Update TestSorted.java
    Seccond

--- TestSorted.java
+++ TestSorted.java
@@ -1,34 +1,36 @@
 package U02;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.NoSuchElementException;
 
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-class TestSorted {
+public class TestSorted {
 
 	
 	Sortable s;
 	
 	@Before
 	public void set(){
-		s = new SortableImpl(new String[][] { {"A","C"},{"B","D"},{"C","E"},{"D","F"}});
+		s = new SortableImpl(new String[][] {{"A","C"},{"B","D"},{"C","E"},{"D","F"}});
 		
 	}
 	
 	@Test
-	public void isWellSorted_firstElement_returnsTrue(){
-		
-		String[] sequence = {"A"};
-		assertEquals(s.isWellSorted(sequence),true);
+	public void isWellSorted_FollowingElements_returnsFalse() {
+		String[] sequence = {"A","B"};
+		assertEquals(s.isWellSorted(sequence),false);
 	}
 	
-	
-	
-	
-	
-	
-	
-	
+    @Test(expected = NoSuchElementException.class)
+	public void Testtest() {
+		String[]sequence = {"A","G"};
+		s.isWellSorted(sequence);
+		
+		
+		
+	}
 
 }
