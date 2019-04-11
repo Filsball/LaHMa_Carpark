716a54c1c24716fb422fc4b828491a2c8e5c1d52
hfreud2s <45362814+hfreud2s@users.noreply.github.com>
2019-04-11 14:26:10 +0200

    :)

---
+++ Sortable.java
@@ -0,0 +1,4 @@
+
+public interface Sortable {
+	public boolean isWellSorted(String[] sequence);
+}
---
+++ SortableImpl.java
@@ -0,0 +1,17 @@
+
+public class SortableImpl implements Sortable {
+	
+	private String[][] tasks;
+	
+	public SortableImpl(String[][] tasks) {
+		this.tasks = tasks;
+	}
+
+	@Override
+	public boolean isWellSorted(String[] sequence) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
+ 
