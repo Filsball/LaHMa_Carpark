b968eae977e8ab1e706d2436491f5eb574432261
LarsHMa <larswiebach@hotmail.de>
2019-06-22 23:06:11 +0200

    Bilanz ver.1

--- LaHMa_Servlet/src/controller/states/BilanzAnzeigeState.java
+++ LaHMa_Servlet/src/controller/states/BilanzAnzeigeState.java
@@ -1,9 +1,16 @@
 package controller.states;
 
+
+
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import controller.LaHMa_Controller;
+import controller.LoeseTicket;
+import model.AboDBS;
+import model.TicketDBS;
 
 public class BilanzAnzeigeState extends ServletState {
 
@@ -14,7 +21,34 @@ public class BilanzAnzeigeState extends ServletState {
 
 	@Override
 	public void processGetRequest(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
+		String req = request.getParameter("method");
+		if (req.equals("Tagesbilanz")) {
+			Date akt = new Date();
+			int sum = TicketDBS.getAbList().stream()
+					.filter(tickets -> tickets.getEnt().getDay()==akt.getDay()
+					&&tickets.getEnt().getMonth()==akt.getMonth()
+					&&tickets.getEnt().getYear()==akt.getYear())
+					.mapToInt(tickets -> tickets.getPreis())
+					.reduce(0, Integer::sum);
+			System.out.println(sum);
+					
+		}else if(req.equals("Monatsbilanz")) {
+			Date akt = new Date();
+			int sum = TicketDBS.getAbList().stream()
+					.filter(tickets -> tickets.getEnt().getMonth()==akt.getMonth()
+					&&tickets.getEnt().getYear()==akt.getYear())
+					.mapToInt(tickets -> tickets.getPreis())
+					.reduce(0, Integer::sum);
+			System.out.println(sum);
+		}else if(req.equals("Jahresbilanz")) {
+			Date akt = new Date();
+			int sum = TicketDBS.getAbList().stream()
+					.filter(tickets -> tickets.getEnt().getYear()==akt.getYear())
+					.mapToInt(tickets -> tickets.getPreis())
+					.reduce(0, Integer::sum);
+			System.out.println(sum);
+			
+		}
 
 	}
 
