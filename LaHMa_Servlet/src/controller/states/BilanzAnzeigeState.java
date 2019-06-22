f04c775444244fd6ad911e00e3906e67c15e499f
LarsHMa <larswiebach@hotmail.de>
2019-06-23 00:03:11 +0200

    Bilanz ver.2

--- LaHMa_Servlet/src/controller/states/BilanzAnzeigeState.java
+++ LaHMa_Servlet/src/controller/states/BilanzAnzeigeState.java
@@ -3,13 +3,12 @@ package controller.states;
 
 
 import java.util.Date;
+import java.util.stream.IntStream;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import controller.LaHMa_Controller;
-import controller.LoeseTicket;
-import model.AboDBS;
 import model.TicketDBS;
 
 public class BilanzAnzeigeState extends ServletState {
@@ -23,30 +22,37 @@ public class BilanzAnzeigeState extends ServletState {
 	public void processGetRequest(HttpServletRequest request, HttpServletResponse response) {
 		String req = request.getParameter("method");
 		if (req.equals("Tagesbilanz")) {
+			int[]hours= new int[24];
 			Date akt = new Date();
-			int sum = TicketDBS.getAbList().stream()
-					.filter(tickets -> tickets.getEnt().getDay()==akt.getDay()
+			IntStream.range(0, 24).forEach( i -> {hours[i] = TicketDBS.getAbList().stream()
+					.filter(tickets -> tickets.getEnt().getYear()==akt.getYear()
 					&&tickets.getEnt().getMonth()==akt.getMonth()
-					&&tickets.getEnt().getYear()==akt.getYear())
+					&&tickets.getEnt().getDate()==akt.getDate()
+					&&tickets.getEnt().getHours()==i)
 					.mapToInt(tickets -> tickets.getPreis())
 					.reduce(0, Integer::sum);
-			System.out.println(sum);
+					System.out.println(hours[i]);});
 					
 		}else if(req.equals("Monatsbilanz")) {
+			int[]days= new int[31];
 			Date akt = new Date();
-			int sum = TicketDBS.getAbList().stream()
-					.filter(tickets -> tickets.getEnt().getMonth()==akt.getMonth()
-					&&tickets.getEnt().getYear()==akt.getYear())
+			IntStream.range(0, 31).forEach( i -> {days[i] = TicketDBS.getAbList().stream()
+					.filter(tickets -> tickets.getEnt().getYear()==akt.getYear()
+					&&tickets.getEnt().getMonth()==akt.getMonth()
+					&&tickets.getEnt().getDate()==i+1)
 					.mapToInt(tickets -> tickets.getPreis())
 					.reduce(0, Integer::sum);
-			System.out.println(sum);
+					System.out.println(days[i]);});
 		}else if(req.equals("Jahresbilanz")) {
+			int[]months= new int[12];
 			Date akt = new Date();
-			int sum = TicketDBS.getAbList().stream()
-					.filter(tickets -> tickets.getEnt().getYear()==akt.getYear())
+			IntStream.range(0, 12).forEach( i -> {months[i] = TicketDBS.getAbList().stream()
+					.filter(tickets -> tickets.getEnt().getYear()==akt.getYear()
+					&&tickets.getEnt().getMonth()==i)
 					.mapToInt(tickets -> tickets.getPreis())
 					.reduce(0, Integer::sum);
-			System.out.println(sum);
+					System.out.println(months[i]);
+			});
 			
 		}
 
